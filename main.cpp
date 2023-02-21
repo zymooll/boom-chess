14fbaf1f71d11f83bf489e91b897c36752df9eb0
zymooll <zymooll@qq.com>
2023-02-21 13:01:06 +0800

    update

--- main.cpp
+++ main.cpp
@@ -2,13 +2,37 @@
 #include <Windows.h>
 #include <stdio.h>
 #include <iostream>
+#include <xstring>
 
 using namespace std;
 using namespace custom;
 
-int main() {
-	Pause("123");
+void Init();
+void Lobby();
 
-	system("pause");
+void Init() {
+
+	return;
+}
+void Lobby() {
+	PrintC("Boom Chess\n1:Play\n2:Help\n0:Exit\n>");
+	while (1) {
+		char opt = getchar();
+		if (opt == '1') {
+			PrintC("1:Local Play\n2:Online Play\n0:Back\n>");
+			while (1) {
+				char opt = getchar();
+			}
+			break;
+		}
+		else if (opt == '2') {
+
+		}
+	}
+}
+
+int main() {
+	Init();
+	Lobby();
 	return 0;
 }
